d14624cef0bdb712cbfa0ec5b13ed8eb2cba4b9e
Asta Rem <asta.rem@hotmail.com>
2024-03-24 21:44:37 +0000

    add layout.module.css, add classes to Layout element

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,13 @@
 import * as React from 'react'
+import Layout from '../components/layout'
+
 
 const AboutPage = () => {
     return (
         <main>
-            <h1>About me</h1>
-            <p>I am learning to create a full stack project with Gatsby.</p>
+            <Layout pageTitle='About me'>
+                <p>I am learning to create a full stack project with Gatsby.</p>
+            </Layout>
         </main>
     )
 }
