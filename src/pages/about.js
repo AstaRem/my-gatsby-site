ce9fdafb80af366aa6466a91fb23b42b7838a242
Asta Rem <asta.rem@hotmail.com>
2024-03-24 19:14:49 +0000

    add about page in source->pages

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <h1>About me</h1>
+            <p>I am learning to create a full stack project with Gatsby.</p>
+        </main>
+    )
+}
+
+export const Head = () => <title>About me</title>
+
+export default AboutPage
